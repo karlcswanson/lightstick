4c14ec7d41b1586f0a1f530a82bd059534063393
Karl Swanson <karlcswanson@gmail.com>
2019-11-20 17:51:08 -0600

    Add LED prototype to repo

--- py/lightstick.py
+++ py/lightstick.py
@@ -1,7 +1,18 @@
 import mido
+import time
+import random
+import board
+import adafruit_dotstar as dotstar
+
 
 KEYBOARD = []
+dots = dotstar.DotStar(board.SCK, board.MOSI, 25, brightness=1)
 
+def random_color():
+    return random.randrange(0, 7) * 32
+
+def color():
+    return (random_color(), random_color(), random_color())
 
 class note:
     def __init__(self, note):
@@ -10,9 +21,13 @@ class note:
 
     def note_on(self, velocity):
         self.velocity = velocity
+        if self.note < 25:
+            dots[self.note] = color()
 
     def note_off(self):
         self.velocity = 0
+        if self.note < 25:
+            dots[self.note] = (0, 0, 0)
 
     def __repr__(self):
         return 'note: {} velocity: {}'.format(self.note, self.velocity)
@@ -25,7 +40,15 @@ def keyboard_init():
 
 def main():
     keyboard_init()
-    inport = mido.open_input()
+
+    inport = mido.open_input('MPKmini2:MPKmini2 MIDI 1 20:0')
+
+    n_dots = len(dots)
+    for dot in range(n_dots):
+        dots[dot] = ( 0, 0, 0)
+
+    time.sleep(.25)
+    print(dots)
     while True:
         msg = inport.receive()
         if msg.type in ['note_on', 'note_off']:
@@ -39,7 +62,7 @@ def main():
 
 
             print(KEYBOARD[msg.note])
-        # print('{}'.format(msg))
+
 
 if __name__ == '__main__':
     main()
