18dd4543100fcf829f964c40b43b940f2bdca08b
Karl Swanson <karlcswanson@gmail.com>
2019-11-20 23:26:42 -0600

    untested changes

--- py/led.py
+++ py/led.py
@@ -1,11 +1,17 @@
 import time
 import random
 
+import numby
 import board
 import adafruit_dotstar as dotstar
+import midi
+
+NOTE_OFF_COLOR = (255, 20, 147)
+NOTE_ON_COLOR = (147, 112, 219)
+ATTACK = 0.5
+RELEASE = 1.0
 
 dots = dotstar.DotStar(board.SCK, board.MOSI, 25, brightness=1)
-
 
 def random_color():
     return random.randrange(0, 7) * 32
@@ -14,18 +20,27 @@ def color():
     return (random_color(), random_color(), random_color())
 
 
-class LED:
-    def __init__(self):
-        pass
 
 def led_init():
     n_dots = len(dots)
     for dot in range(n_dots):
-        dots[dot] = ( 0, 0, 0)
+        dots[dot] = (0, 0, 0)
 
     time.sleep(.25)
     print(dots)
 
+
+def calc_color(starting_color, target_color, ratio):
+    return starting_color + ratio * numby.subtract(target_color, starting_color)
+
+def update_colors():
+    for key in midi.KEYBOARD:
+        if key.velocity > 0:
+            dots[key.note] = calc_color(NOTE_OFF_COLOR, NOTE_ON_COLOR, key.ratio(ATTACK))
+        if key.velocity == 0:
+            dots[key.note] = calc_color(NOTE_ON_COLOR, NOTE_OFF_COLOR, key.ratio(RELEASE))
+
 def led_loop():
     while True:
+        update_colors()
         time.sleep(.05)
--- py/midi.py
+++ py/midi.py
@@ -9,43 +9,42 @@ KEYBOARD = []
 
 inport = mido.open_input('MPKmini2:MPKmini2 MIDI 1 20:0')
 
-NOTE_ON_COLOR = ( 147, 112, 219)
-ATTACK = 0.5
-RELEASE = 1.0
-NOTE_OFF_COLOR = ( 255, 20, 147)
 
 class note:
     def __init__(self, note):
         self.note = note
         self.velocity = 0
-        
-        self.target_brightness = 0
         self.timestamp = time.time()
 
     def note_on(self, velocity):
         self.velocity = velocity
-        self.target_brightness = velocity * 2
-        if self.note < 25:
-            led.dots[self.note] = (255, 0, 0, random.random())
+        self.timestamp = time.time()
+
 
     def note_off(self):
         self.velocity = 0
-        # self.target_brightness
-        if self.note < 25:
-            led.dots[self.note] = (0, 0, 0)
+        self.timestamp = time.time()
+
+
+    def ratio(self, atk_rel_time):
+        out = (time.time() - self.timestamp)/atk_rel_time
+        if out > 1:
+            return 1
+        return out
 
     def __repr__(self):
         return 'note: {} velocity: {}'.format(self.note, self.velocity)
 
 
 def keyboard_init():
-    for i in range (0, 87):
+    # for i in range (0, 87):
+    for i in range(0, 24):
         KEYBOARD.append(note(i))
     print(KEYBOARD)
 
-    
 
-def midi_loop():  
+
+def midi_loop():
     while True:
         msg = inport.receive()
         if msg.type in ['note_on', 'note_off']:
